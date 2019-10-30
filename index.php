8e4e1e4eb701c8f09ff278ed633dee9e74e78bed
Nisefal <kolyabogdanenko@gmail.com>
2019-10-30 19:12:00 +0200

    RSS and turbo-pages
    Moved script code to another file

--- index.php
+++ index.php
@@ -4,22 +4,3 @@ session_start();
 //ini_set('display_errors', 1);
 
 require_once 'application/bootstrap.php';
-
-
-
-//ниже турбостраницы генерятся и rss лента - вынести на крон
-
-/*
-include_once 'application/models/model_csv.php';
-include_once 'application/models/model_rss.php';
-
-
-
-
-$model_csv = new Model_csv();
-$model_csv->add_csv();
-
-
-$model_rss = new Model_rss();
-$model_rss->add_rss();a
-*/
---
+++ script.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+include_once 'application/models/model_csv.php';
+include_once 'application/models/model_rss.php';
+
+$model_csv = new Model_csv();
+$model_csv->add_csv();
+
+
+$model_rss = new Model_rss();
+$model_rss->add_rss();
